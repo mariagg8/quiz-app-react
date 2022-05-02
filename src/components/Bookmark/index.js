f0e3b1545e94189f3b5db7d6b15845343b982bcb
Maria Gonzalez <chelo140581@gmail.com>
2022-05-02 19:28:10 +0200

    add bookmark via Lifting up state

--- src/components/Bookmark/index.js
+++ src/components/Bookmark/index.js
@@ -1,3 +1,11 @@
-export default function Bookmark() {
-  return <button className="bookmark-icon-active"></button>;
+import './style.css';
+
+export default function Bookmark({ checked, onToggle }) {
+  return (
+    <div
+      data-js="bookmark-toggle"
+      className={checked ? 'bookmark-icon-active' : 'bookmark-icon'}
+      onClick={onToggle}
+    ></div>
+  );
 }
